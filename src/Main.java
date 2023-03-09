57eef14e2ad49c88aabe639722a998bf4be1e643
AndreiPetoukhov1986 <oziglena@mail.ru>
2023-03-09 17:04:35 +0300

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car car[] = {
+                new Car("car1",4),
+                new Car("car2",4)
+        };
+
+        Truck truck[] = {
+                new Truck("truck1",6),
+                new Truck("truck2",8)
+        };
+
+        Bicycle bicycle[] = {
+                new Bicycle("bicycle1",2),
+                new Bicycle("bicycle2",2)
+        };
+
+        Vehicles vehicles[] = new Vehicles[car.length+ truck.length+ bicycle.length];
+        for (int i = 0; i < car.length; i++) {
+            vehicles[i]=car[i];
+        }
+        for (int i = 0; i < truck.length; i++) {
+            vehicles[i+ car.length]=truck[i];
+        }
+        for (int i = 0; i < bicycle.length; i++) {
+            vehicles[i + car.length + truck.length] = bicycle[i];
+        }
+
+        ServiceStation station = new ServiceStation();
+        for (int i = 0; i < vehicles.length; i++) {
+            station.checkStation(vehicles[i]);
+        }
+
     }
 }
